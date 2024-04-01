d4582e89d1d63fc53731bebfe23fe6cdbb348aa4
sumaiyasheikh7650 <sumaiyaakter1689@gmail.com>
2024-04-01 11:48:33 +0600

    first commit

---
+++ elesis.js
@@ -0,0 +1,18 @@
+var  borkapraice = 50;
+var dressprice = 20;
+var hijabprice = 10;
+var mybugget = 70;
+
+
+if(borkaprice<mybugget){
+    console.log('borka kinbo');
+}else if(dresspriceprice<mybugget){
+    console.log('drees kinbo');
+
+
+}else if(hijabprice<mybugget){
+    console.log('hijab kinbo');
+}
+    else{
+        console.log(" al pin kinbo");
+    }
